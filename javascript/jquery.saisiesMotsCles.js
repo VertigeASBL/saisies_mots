82be9e640e75292b03768d00d6f880ae92b3fcfc
Michel Bystranowski <michel@vertige.org>
2015-01-22 12:49:43 +0100

    fieldsets à tiroir simple(iste)…

--- javascript/jquery.saisiesMotsCles.js
+++ javascript/jquery.saisiesMotsCles.js
@@ -2,13 +2,24 @@
     // définition du plugin jquery
     $.fn.saisiesMotsCles = function (options) {
 
-        var config;
+        var config, saisie = $(this);
 
         config = $.extend(true, {}, options);
 
-        console.log(this);
+        saisie.find('fieldset').each(function () {
+            var fieldset = $(this),
+                legend   = fieldset.children('legend'),
+                contenu  = fieldset.children('.contenu');
 
-        return this;
+            // contenu.hide();
+            legend.click(function () {
+                contenu.toggle({
+                    duration: 200
+                });
+            });
+        });
+
+        return saisie;
     };
 
     // On l'applique aux saisies mots_cles_arborescents
