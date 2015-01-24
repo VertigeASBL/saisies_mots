6d35e0fbda59dc544a2d36acc1c9b65f89a86f52
Michel Bystranowski <bystrano@gmx.ch>
2015-01-24 21:13:21 +0100

    les mots-clés correspondants à un groupe servent de raccourci pour activer ou désactiver tous les mots-clés du groupe

--- javascript/jquery.saisiesMotsCles.js
+++ javascript/jquery.saisiesMotsCles.js
@@ -2,7 +2,7 @@
     // définition du plugin jquery
     $.fn.saisiesMotsCles = function (options) {
 
-        var config, saisie = $(this);
+        var config, saisie = $(this), groupes;
 
         config = $.extend(true, {
             demarrer_plie: true
@@ -22,6 +22,45 @@
                 contenu.toggle({
                     duration: 200
                 });
+            });
+        });
+
+        // On calcule une fois pour toutes une liste des groupes de
+        // mots-clés
+        groupes = saisie.find('.choix_groupe_mots');
+        groupes = $.map(groupes, function (el) {
+            return {
+                'id': el.className.replace(/^.*choix_groupe_([^ ]+).*$/,'$1'),
+                'titre': $(el).find('legend').html().trim()
+            };
+        });
+
+        // les mot-clés qui ont le même titre qu'un groupe sont
+        // spéciaux, quand on les active/désactive, on
+        // active/désactive automatiquement les mots-clés du groupe
+        // correspondant.
+        saisie.find('.choix_mot').each(function () {
+            var choix_mot = $(this),
+                titre = choix_mot.find('label').html().trim();
+
+            $.each(groupes, function (i,groupe) {
+
+                if (titre === groupe.titre) {
+
+                    choix_mot.change(function (e) {
+                        if (e.target.checked) {
+                            $('.choix_groupe_' + groupe.id + ' input')
+                                .attr('checked','checked')
+                                .trigger('change');
+                        } else {
+                            $('.choix_groupe_' + groupe.id + ' input')
+                                .attr('checked', false)
+                                .trigger('change');
+                        }
+                    });
+
+                    return false;
+                }
             });
         });
 
