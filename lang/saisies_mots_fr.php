49f11607fbe35f478822914dfc7a579eaead22a0
Michel Bystranowski <michel@vertige.org>
2016-04-18 16:55:19 +0200

    ajoute un fichier yaml à la saisie checkbox_mots
    pour pouvoir l'utiliser dans les formulaires formidable

--- lang/saisies_mots_fr.php
+++ lang/saisies_mots_fr.php
@@ -3,6 +3,12 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// O
+	'option_id_groupe_explication' => 'Un éventuel groupe de mot-clés auquel restreindre la sélection',
+	'option_id_groupe_label' => 'Groupe de mots-clés',
+
 	// S
+	'saisie_checkbox_mots_explication' => 'Des cases à cocher reprenant les mots-clés du site',
+	'saisie_checkbox_mots_titre' => 'Mots-clés à cocher',
 	'saisies_mots_titre' => 'Saisies pour Mots-Clés',
 );
