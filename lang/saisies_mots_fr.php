f08ef492fef550f59833e10f0281de10acf0384b
Michel Bystranowski <michel@vertige.org>
2016-04-18 16:40:31 +0200

    règles de codage

--- lang/saisies_mots_fr.php
+++ lang/saisies_mots_fr.php
@@ -1,10 +1,8 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-    // S
-    'saisies_mots_titre' => 'Saisies pour Mots-Clés',
+	// S
+	'saisies_mots_titre' => 'Saisies pour Mots-Clés',
 );
